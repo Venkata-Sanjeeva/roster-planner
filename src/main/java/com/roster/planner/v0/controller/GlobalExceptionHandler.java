4c1bd267d18c9165e104f534231a60d83e11b567
Venkata-Sanjeeva <kuntumallavenkatasanjeeva2005@gmail.com>
2026-09-21 21:21:06 +0530

    feat(slf4j): Added SLF4J Logging to the Global Exception Handler

--- src/main/java/com/roster/planner/v0/controller/GlobalExceptionHandler.java
+++ src/main/java/com/roster/planner/v0/controller/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.roster.planner.v0.exception.*;
 import com.roster.planner.v0.response.ErrorResponse;
 import com.roster.planner.v0.response.GlobalResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -134,6 +136,13 @@ public class GlobalExceptionHandler {
     // Generic fallback for any other unexpected errors (500)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<GlobalResponse<ErrorResponse>> handleGlobalException(Exception ex, HttpServletRequest request) {
+        log.error(
+                "Unhandled exception. Method={}, URI={}",
+                request.getMethod(),
+                request.getRequestURI(),
+                ex
+        );
+
         ErrorResponse error = new ErrorResponse(
             LocalDateTime.now(),
             "An unexpected error occurred",
