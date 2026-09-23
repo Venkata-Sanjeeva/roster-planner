f084b24bcf8bdcf44984400c448d9194963f6034
Venkata-Sanjeeva <kuntumallavenkatasanjeeva2005@gmail.com>
2026-09-23 21:38:29 +0530

    feat(ResourceNotFound): Added NoResourceFoundException.

--- src/main/java/com/roster/planner/v0/controller/GlobalExceptionHandler.java
+++ src/main/java/com/roster/planner/v0/controller/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -117,6 +118,28 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<GlobalResponse<ErrorResponse>> handleNoResourceFoundException(
+            NoResourceFoundException ex,
+            HttpServletRequest request) {
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+
+        GlobalResponse<ErrorResponse> response = GlobalResponse.<ErrorResponse>builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message("The requested resource was not found.")
+                .data(error)
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
     }
 
     @ExceptionHandler(NullPointerException.class)
